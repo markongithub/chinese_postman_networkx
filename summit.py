0f3dd326db2c55094c0bda38857936cec6a6de39
mark on github <markongithub@users.noreply.github.com>
2017-07-04 19:00:12 +0000

    Comment node purges and eliminate old debugging code.

--- summit.py
+++ summit.py
@@ -18,7 +18,15 @@ def summit_fixes(g):
   for node in g:
     streets = cpl.adjoining_streets(g, node)
     lon, lat = g.node[node].get('coordinate')
+    # All of these are points where streets cross into other towns, and I
+    # eliminate points in order to cut off routes into other towns.
+    # I am writing these in clockwise order from where Morris Avenue crosses
+    # NJ-24 into Springfield.
     if (('Morris Avenue' in streets and lon > -74.331090) or
+        # What lines of this form are saying is that you can be on the
+        # intersection of the two, but you can't otherwise be on Briant Park
+        # Drive. If I eliminated ALL Briant Park Drive points, I'd be severing
+        # Springfield Avenue and disconnecting parts of Summit.
         ('Briant Park Drive' in streets and 'Springfield Avenue' not in streets) or
         ('Orchard Street' in streets and lon > -74.338021) or
 #        ('McCallum Street' in streets and lat < 40.054860) or
@@ -29,9 +37,11 @@ def summit_fixes(g):
         ('Glenside Avenue' in streets and lat < 40.688211) or
         ('Winchip Road' in streets and lon < -74.381843) or
         ('Glenside Avenue' in streets and lat < 40.688211) or
+        # If you're going west on Club Drive, Club Lane is where you can assume
+        # you've left Summit.
         ('Club Lane' in streets) or
         ('Mountain Avenue' in streets and lon < -74.384198) or
-        # See below for Division Avenue border.
+        # See below for the Division Avenue border.
         ('Springfield Avenue' in streets and lon < -74.387309) or
         ('Mount Vernon Avenue' in streets and lon < -74.391220) or
         ('Stanley Avenue' in streets and lat > 40.726245) or
@@ -41,27 +51,27 @@ def summit_fixes(g):
         ('Main Street' in streets and lon < -74.371891) or
         ('Hobart Gap Road' in streets and 'Hobart Avenue' not in streets) or
         ('Overhill Road' in streets and 'Mountain Avenue' not in streets) or
-        # This isn't in the tax map so I am declaring it not a real street.
+        # Old Coach isn't in the tax map so I am declaring it not a real street.
         ('Old Coach Road' in streets and 'Baltusrol Road' not in streets) or
         # See below for Morris Turnpike border.
         ('Morris Turnpike' in streets and lon > -74.331790)):
        nodes_to_purge.append(node)
     elif 'Division Avenue' in streets:
+      # Anything emerging from Division Avenue to the west is in Berkeley
+      # Heights or New Providence. Except for that one stretch of Old
+      # Avenue.
       for neighbor in g[node]:
         neighbor_streets = cpl.adjoining_streets(g, neighbor)
         if 'Division Avenue' not in neighbor_streets and 'Old Springfield Avenue' not in neighbor_streets and cpl.longitude(g, neighbor) < lon:
           print "I will purge node %s (%s,%s) on %s because it is west from Division Avenue node %s (%s,%s)." % (neighbor, cpl.latitude(g, neighbor), cpl.longitude(g, neighbor), neighbor_streets, node, lat, lon)
           nodes_to_purge.append(neighbor)
     elif 'Morris Turnpike' in streets:
+      # Anything emerging from Morris Turnpike to the north is Springfield or
+      # Millburn.
       for neighbor in g[node]:
         neighbor_streets = cpl.adjoining_streets(g, neighbor)
         if 'Morris Turnpike' not in neighbor_streets and cpl.latitude(g, neighbor) > lat:
           print "I will purge node %s (%s,%s) on %s because it is north from Morris Turnpike node %s (%s,%s)." % (neighbor, cpl.latitude(g, neighbor), cpl.longitude(g, neighbor), neighbor_streets, node, lat, lon)
-          nodes_to_purge.append(neighbor)
-    elif 'Northwestern Avenue' in streets:
-      for neighbor in g[node]:
-        if 'Northwestern Avenue' not in cpl.adjoining_streets(g, neighbor) and cpl.longitude(g, neighbor) < lon:
-          print "I will purge a node on %s because it is west from Northwestern." % cpl.adjoining_streets(g, neighbor)
           nodes_to_purge.append(neighbor)
 
   for node in set(nodes_to_purge):
@@ -73,11 +83,6 @@ def summit_fixes(g):
   non_town_nodes = set(g.nodes()) - town_nodes
   for node in non_town_nodes:
     g.remove_node(node)
-
-  fellsway_edges = [(n1, n2) for (n1, n2) in g.edges() if g[n1][n2].get('name') == 'The Fellsway']
-  if fellsway_edges:
-    f_node = fellsway_edges[0][0]
-    print "Your leak is in here: %s" % [g.node[node]['pretty_name'] for node in networkx.shortest_path(g, 2522233328, f_node)]
 
 summit_filename="/home/mark/git/chinese_postman/data/summit_all.osm"
 
